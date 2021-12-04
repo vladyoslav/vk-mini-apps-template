7745ef2179f0d00d82dc1bc5750702cf6fa62039
VladYoSlav <vaifaer@gmail.com>
2021-12-04 21:28:01 +0300

    Migrated from CRA to Vite

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+export default defineConfig({
+  plugins: [reactRefresh()],
+  server: {
+    port: 10888,
+    https: true
+  }
+})
