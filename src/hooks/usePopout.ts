93628df24f0884ac37c7d77e172ba356664cd4e3
VladYoSlav <vaifaer@gmail.com>
2021-12-04 21:28:01 +0300

    Fixed React hooks rules violation

--- src/hooks/usePopout.ts
+++ src/hooks/usePopout.ts
@@ -1,5 +1,4 @@
-import { useSetAtomState } from '@mntm/precoil'
 import { popoutAtom } from '../store/atoms'
 
-export const setPopout = useSetAtomState(popoutAtom)
+export const setPopout = popoutAtom.set
 export const closePopout = () => setPopout(undefined)
